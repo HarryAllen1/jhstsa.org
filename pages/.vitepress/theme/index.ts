5ceaea2487d8294120dd7cd064f31be117888a6e
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-08-24 22:39:11 -0700

    colour stuff

--- pages/.vitepress/theme/index.ts
+++ pages/.vitepress/theme/index.ts
@@ -1,4 +1,3 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
@@ -6,10 +5,6 @@ import './style.css';
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
-  },
+  Layout: () => h(DefaultTheme.Layout, null, {}),
   enhanceApp() {},
 } satisfies Theme;
